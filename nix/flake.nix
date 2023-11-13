eeaad5bd6275eec279bae1505dc32f9fa7714ffb
haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-11-14 00:24:14 +0900

    add flake.nix

---
+++ nix/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+	pkgs = import nixpkgs { inherit system; };
+
+	nodejs = pkgs.nodejs;
+	yarn = pkgs.yarn;
+      in
+      {
+        devShell = pkgs.mkShell {
+	  buildInputs = [
+	    nodejs
+	    yarn
+	    python-ldap
+          ];
+	};
+      });
+}
