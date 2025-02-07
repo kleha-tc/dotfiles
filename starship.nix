121853ecd88947c012ebc2d715c6bc8f13010392
haru-0205 <rm23081b@st.omu.ac.jp>
2025-02-08 01:49:43 +0900

    fix format

--- starship.nix
+++ starship.nix
@@ -5,9 +5,13 @@
 		settings = {
 			add_newline = true;
 			format = ''
-[┌─────────────](bold green)$directory$git_branch$git_commit$git_metrics$git_status
-[│](bold green) $all
-[└──>](bold green)$character'';
+[┌──────────](bold green)$directory$git_branch$git_commit$git_metrics$git_status
+[│](bold green) 
+[└─](bold green)$character'';
+			character = {
+				success_symbol = "[─>>](bold green)"
+				error_symbol = "[─|>](bold red)"
+			};
 		};
 	};
 }
