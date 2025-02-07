c5bf85525390a15b7bd77a7e555413f3b217ba10
haru-0205 <rm23081b@st.omu.ac.jp>
2025-02-08 01:12:18 +0900

    fix configuration

--- starship.nix
+++ starship.nix
@@ -3,12 +3,11 @@
 	programs.starship = {
 		enable = true;
 		settings = {
-			"$schema" = 'https://starship.rs/config-schema.json'
-			add_newline = true
+			add_newline = true;
 			format = '''
 			[┌─────────────](bold green) $directory$git
 			[│](bold green) $all
-			[└──>](bold green)'''
+			[└──>](bold green)''';
 		};
 	};
-}
+};
