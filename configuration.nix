0120d3b374eb497609e23019e6d0f22c9bad5cd0
Kleha <rm23081b@st.omu.ac.jp>
2025-03-11 14:11:27 +0900

    fix configuration

--- configuration.nix
+++ configuration.nix
@@ -4,29 +4,6 @@
 
 { config, pkgs, ... }:
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    rtl88xxau-aircrack
-  ];
-
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.wireless.iwd.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
 
@@ -45,116 +22,27 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  # services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  # services.displayManager.sddm = {
-  #   enable = true;
-	#   wayland.enable = true;
-  # };
-  # services.desktopManager.plasma6.enable = true;
-
-	# Enable Budgie Desktop Environment
-
-	# services.xserver.desktopManager.budgie.enable = true;
-
-	# Enable polkit
-	security.polkit.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-	# Enable Nvidia GPU support
-
-	hardware.graphics = {
-		enable = true;
-	};
-
-	services.xserver.videoDrivers = ["nvidia"];
-
-	hardware.nvidia = {
-		modesetting.enable = true;
-		powerManagement.enable = false;
-		powerManagement.finegrained = false;
-		open = false;
-		nvidiaSettings = true;
-		package = config.boot.kernelPackages.nvidiaPackages.stable;
-		prime = {
-			sync.enable = true;
-			intelBusId = "PCI:0:2:0";
-			nvidiaBusId = "PCI:1:0:0";
-		};
-	};
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kleha = {
     isNormalUser = true;
-    description = "Kleha";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    description = "WSL Default User";
+    extraGroups = [ "wheel" "docker" ];
     packages = with pkgs; [
-      kdePackages.kate
-      thunderbird
-      teams-for-linux
-      discord
-      slack
-      onlyoffice-desktopeditors
-      libreoffice
-      freecad-wayland
-      blender
-      openscad
-      gimp
-      inkscape
-      zrythm
-      typst
-      satysfi
-      obsidian
+			typst
 			tree-sitter
 			ngspice
-			bottles
-			kdePackages.kdeconnect-kde
-			kdePackages.okular
-			alacritty
-			realvnc-vnc-viewer
 		];
     shell = pkgs.zsh;
   };
-
-  # Install firefox.
-  programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
 	# Enable Nix-ld
-
 	programs.nix-ld.enable = true;
+
+	# Enable zsh
+	programs.zsh.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -163,31 +51,11 @@
     wget
     libskk
     skktools
-    fcitx5-configtool
-    obs-studio
-    win-virtio
-    deno
 		gcc
 		docker
 		trashy
-		vulkan-tools
-		usbutils
-		ghostty
   ];
 
-  programs = {
-    neovim = {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-    };
-    zsh = {
-      enable = true;
-    };
-  };
-
-	
   fonts = {
     packages = with pkgs; [
       ipaexfont
@@ -204,75 +72,18 @@
       };
     };
   };
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   # Enable experimental features
-
   nix = {
     settings = {
       experimental-features = ["nix-command" "flakes"];
     };
   };
 
-  # internationalization (i18n)
-
-  i18n.inputMethod = {
-    type = "fcitx5";
-    enable = true;
-    fcitx5.addons = with pkgs; [fcitx5-skk];
-    fcitx5.waylandFrontend = true;
-  };
-
-  fonts = {
-    packages = with pkgs; [
-      ipaexfont
-      noto-fonts-emoji
-      hackgen-nf-font
-    ];
-    fontDir.enable = true;
-    fontconfig = {
-      defaultFonts = {
-        serif = ["IPAexMincho" "Noto Color Emoji"];
-        sansSerif = ["IPAexGothic" "Noto Color Emoji"];
-        monospace = ["HackGen Console NF" "Noto Color Emoji"];
-        emoji = ["Noto Color Emoji"];
-      };
-    };
-  };
-
   # VM
-  
   virtualisation = {
 		docker.enable = true;
-    libvirtd = {
-      enable = true;
-      qemu = {
-        swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
-      };
-    };
-    spiceUSBRedirection.enable = true;
   };
-  users.groups.libvirtd.members = [ "kleha" ];
-  programs.virt-manager.enable = true;
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- flake.nix
+++ flake.nix
@@ -1,16 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, nixos-wsl, ... }: {
     nixosConfigurations = {
       kleha-nix = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+					nixos-wsl.nixosModules.default {
+						# Enable NixOS-WSL Module
+						wsl.enable = true;
+						wsl.defaultUser = "kleha";
+					}
+					./configuration.nix
 					home-manager.nixosModules.home-manager
 					{
 						home-manager.useGlobalPkgs = true;
