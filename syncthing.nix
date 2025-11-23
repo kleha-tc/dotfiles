3670f00b75b0ecac38b9f5337d72932755e4e8ce
kleha <kleha@klefpj.org>
2025-11-23 22:51:20 +0900

    Add Syncthing User-Env Settings

--- syncthing.nix
+++ syncthing.nix
@@ -3,5 +3,31 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    user = "kleha";
+		settings = {
+      devices = {
+        "SC-53C" = { id = "7IHYNKN-PNA26AJ-FTXD4XK-SLCL26Q-ZATCRVB-DHBNAPI-NAHSBJI-VL6XBQQ"; };
+      };
+			folders = {
+				"/home/kleha/Documents/syncthing/keepass" = {
+					id = "keepass";
+          devices = [ "SC-53C" ];
+				};
+				"/home/kleha/working/klef-learning/cource" = {
+					id = "klef-learning-cource";
+          devices = [ "SC-53C" ];
+				};
+				"/home/kleha/knoledge/org" = {
+					id = "knowledge-org";
+          devices = [ "SC-53C" ];
+				};
+				"/home/kleha/knowledge/obsidian" = {
+					id = "knoledge-obsidian";
+          devices = [ "SC-53C" ];
+				};
+			};
+		};
+		dataDir = "/home/kleha";
+		configDir = "/home/kleha/.config/syncthing";
   };
 }
